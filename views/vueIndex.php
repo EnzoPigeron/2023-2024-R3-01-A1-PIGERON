ddc1df7cf89820cf61175b98c012c2622b78a70e
EnzoPigeron <134093904+EnzoPigeron@users.noreply.github.com>
2023-11-16 11:56:15 +0100

    [views\vueIndex.php] TP05 - 2.3 Ajout du require once

--- views/vueIndex.php
+++ views/vueIndex.php
@@ -1,27 +1,31 @@
+<?php
+    require_once 'View.php';
+?>
+
 <!doctype html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="public/css/main.css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $titre ?></title>
-</head>
-
-<body>
-<header>
-    <!-- Menu -->
-    <nav>
-
-    </nav>
-</header>
-<!-- #contenu -->
-<main id="contenu">
-    <h1>Pokédex de <?= $nomDresseur ?></h1>
-</main>
-<footer>
-
-</footer>
-</body>
-
-</html>
+    <html lang="fr">
+    
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="public/css/main.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= $titre ?></title>
+    </head>
+    
+    <body>
+    <header>
+        <!-- Menu -->
+        <nav>
+    
+        </nav>
+    </header>
+    <!-- #contenu -->
+    <main id="contenu">
+        <h1>Pokédex de <?= $nomDresseur ?></h1>
+    </main>
+    <footer>
+    
+    </footer>
+    </body>
+    
+    </html>   
