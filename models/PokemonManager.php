7f1e7027d303d1e2b6f0abef9bed242c443c6e0a
EnzoPigeron <134093904+EnzoPigeron@users.noreply.github.com>
2023-11-27 16:51:29 +0100

    [PokemonManager.php/getAll,getById] - TP06 | 2.2 Ajout de PokemonManager avec les méthodes getAll et getById

---
+++ models/PokemonManager.php
@@ -0,0 +1,39 @@
+<?php
+    require_once 'models/Model.php';
+    require_once 'models/Pokemon.php';
+
+    /**
+     * Classe PokemonManager
+     */
+    class PokemonManager extends Model {
+        public function __construct() {
+            parent::__construct();
+        }    
+
+        /**
+         * Renvoie tous les pokemons 
+         * @return array un tableau de pokemon
+         */
+        public function getAll() : array {
+            $res = $this->execRequest('SELECT * FROM pokemon');
+            $res = $res->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
+        }
+
+        /**
+         * Renvoie un pokemon a partir de son id
+         * @param int $idPokemon l'id du pokemon 
+         * @return ?Pokemon renvoie un objet Pokemon
+         */
+        public function getById(int $idPokemon) : ?Pokemon {
+            $res = $this->execRequest('SELECT * FROM pokemon WHERE idPokemon=?',array($idPokemon));
+            $res = $res->fetch(PDO::FETCH_ASSOC);
+            if ($res) {
+                $pokemon = new Pokemon($res['idPokemon'],$res['nomEspece'],$res['description'],$res['typeOne'],$res['typeTwo'],$res['urlImg']);
+            } else {
+                $pokemon = null;
+            }
+            return $pokemon;
+        }
+    }
+?>
