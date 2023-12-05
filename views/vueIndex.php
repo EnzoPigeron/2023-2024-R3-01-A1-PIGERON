342bbb33e1edf0f8ae45f1bc33ac651d71ed6694
EnzoPigeron <134093904+EnzoPigeron@users.noreply.github.com>
2023-12-05 09:00:52 +0100

    [vueIndex.php] - TP06 | 3.1 Ajout d'un tableau pour afficher les pokemon

--- views/vueIndex.php
+++ views/vueIndex.php
@@ -5,12 +5,12 @@
 <!doctype html>
     <html lang="fr">
     
-    <head>
+<head>
         <meta charset="UTF-8"/>
         <link rel="stylesheet" href="public/css/main.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $titre ?></title>
-    </head>
+</head>
     
     <body>
     <header>
@@ -28,10 +28,33 @@
         <p><?php var_dump($first); ?></p>
 
         <p><?php var_dump($other); ?></p>
+
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Pokemon</th> 
+                <th>Description</th>
+                <th>Type(s)</th>
+                <th>Image</th>
+            </tr>
+            <?php
+            foreach ($listPokemon as $pokemon) {
+                echo ("
+                <tr>
+                    <td>$pokemon[idPokemon]</td>
+                    <td>$pokemon[nomEspece]</td>
+                    <td>$pokemon[description]</td>
+                    <td>$pokemon[typeOne] $pokemon[typeTwo]</td>
+                    <td><img src=$pokemon[urlImg] alt=$pokemon[nomEspece]></td>
+                </tr>
+                ");
+            }
+            ?>            
+        </table>
     </main>
     <footer>
     
     </footer>
     </body>
     
-    </html>   
+</html>   
