317992f981cd2ef642a2d51ed5a0dd15e61672ca
EnzoPigeron <134093904+EnzoPigeron@users.noreply.github.com>
2024-01-09 06:53:24 +0100

    TP07 | 3.2 Debut du formulaire de l'ajout de type

--- views/vueAddType.php
+++ views/vueAddType.php
@@ -1,3 +1,22 @@
 <?php
     echo("add type");
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un type</title>
+</head>
+<body>
+    <form>
+        <fieldset> <legend>Ajouter un type </legend>
+            <div>
+                <label for="Name">Nom </label>
+                <input type="text" name="Name" required>
+            </div>
+        </fieldset>
+    </form>  
+</body>
+</html>
