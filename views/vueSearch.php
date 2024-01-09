92d224352cdf34c02a4395db1c1ca33b84e1ff2a
enzopigeron <enzo.pigeron@iut-dijon.u-bourgogne.fr>
2024-01-09 08:11:05 +0100

    TP07 | 3.3 Formulaire de la page de recherche

--- views/vueSearch.php
+++ views/vueSearch.php
@@ -1,3 +1,31 @@
 <?php
     echo("serach pokemon");
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recherche</title>
+</head>
+<body>
+    <form>
+        <fieldset> <legend>Rechercher </legend>
+            <div>
+                <label for="Search">Recherche </label>
+                <input type="text" name="Search" required>
+            </div>
+            <div>
+                <label for="Champ">Champ de recherche</label>
+                <select name="Champ" required>
+                    <option>Nom</option>
+					<option>Description</option>
+					<option>Type(s)</option> 
+                </select>   
+            </div>
+        </fieldset>
+    </form>  
+</body>
+</html>
